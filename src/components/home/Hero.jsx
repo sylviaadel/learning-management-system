c2aa7e1ca684c7d13890a34d4bdc2471994abbcf
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-16 01:48:55 +0100

    Add hero section and basic styles

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section className="Hero">
+      <h1>Do you want to learn how to be a Fashion Icon?</h1>
+      <p>
+        Be your own personal styler, Master your image boost your confidence!
+      </p>
+      <button className="primary-btn">Join Course</button>
+    </section>
+  );
+}

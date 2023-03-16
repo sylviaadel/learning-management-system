e1a30667333e6487122bad6ee86966b3e3aef934
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-16 18:36:43 +0100

    Add the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
 import "./styles/style.scss";
+import Navbar from "./components/shared/Navbar";
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,33 @@
+import logo from "../../assets/images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, brands } from "@fortawesome/fontawesome-svg-core/import.macro";
+
+export default function Navbar() {
+  const logoAlt = "A mannequin with red dress and a pen beside it";
+
+  return (
+    <nav id="Navbar">
+      <ul>
+        <li>
+          <img src={logo} alt={logoAlt} />
+          <span className="link-text">Fabric Dreem</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={solid("calendar-days")} />
+          <span className="link-text">Check Calendar</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={brands("slack")} />
+          <span className="link-text">Open in Slack</span>
+        </li>
+        <li className="login-btn">
+          <FontAwesomeIcon icon={solid("circle-user")} />
+          <span className="link-text">Logout</span>
+        </li>
+        <li className="login-btn">
+          <FontAwesomeIcon icon={solid("angles-right")} />
+        </li>
+      </ul>
+    </nav>
+  );
+}

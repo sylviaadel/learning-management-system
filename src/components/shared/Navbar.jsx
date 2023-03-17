4a6a5995a337bf83ab186a264aaa9539184550b0
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-17 23:26:38 +0100

    refactor navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,8 +1,7 @@
 import logo from "../../assets/images/logo.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid, brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { useUser } from "../../state/UserProvider";
 import { useNavigate } from "react-router";
+import navLinks from "../../data/navLinks.json";
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -15,6 +14,15 @@ export default function Navbar() {
     navigate("/");
   }
 
+  const Links = navLinks.map((item) => (
+    <li key={item.title}>
+      <a href={item.link}>
+        <i className={item.icon}></i>
+        <span className="link-text">{item.title}</span>
+      </a>
+    </li>
+  ));
+
   return (
     <nav id="Navbar">
       <ul>
@@ -22,24 +30,13 @@ export default function Navbar() {
           <img src={logo} alt={logoAlt} />
           <span className="link-text">Fabric Dreem</span>
         </li>
-        <li>
-          <a href="https://calendar.google.com/calendar/u/0/r">
-            <FontAwesomeIcon icon={solid("calendar-days")} />
-            <span className="link-text">Check Calendar</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://slack.com/">
-            <FontAwesomeIcon icon={brands("slack")} />
-            <span className="link-text">Open in Slack</span>
-          </a>
-        </li>
+        {Links}
         <li className="login-btn" onClick={() => onLogout()}>
-          <FontAwesomeIcon icon={solid("circle-user")} />
+          <i className="fa-solid fa-user"></i>
           <span className="link-text">Logout</span>
         </li>
         <li className="expand-btn">
-          <FontAwesomeIcon icon={solid("angles-right")} />
+          <i className="fa-solid fa-angles-right"></i>
         </li>
       </ul>
     </nav>

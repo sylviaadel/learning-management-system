4a94964d0307cfa23dd81d8209007502ea851804
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-18 10:23:21 +0100

    fix nav issue

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
   const Links = navLinks.map((item) => (
     <li key={item.title}>
-      <a href={item.link}>
+      <a href={item.link} target="_blank">
         <i className={item.icon}></i>
         <span className="link-text">{item.title}</span>
       </a>

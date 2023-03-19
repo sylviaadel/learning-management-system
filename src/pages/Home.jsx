04c4f12a2a67e8f8bef6899c8740c8bfe7a4cf63
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-20 00:55:16 +0100

    Add special features section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/home/Hero";
+import SpecialFeatures from "../components/home/SpecialFeatures";
 
 export default function Home() {
   return (
     <div id="Home">
       <Hero />
+      <SpecialFeatures />
     </div>
   );
 }

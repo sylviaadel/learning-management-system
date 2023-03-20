ef5ede1abd802d823949f14f37848a04e45180a0
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-20 21:54:15 +0100

    Add read links

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useCourse } from "../state/CoursesProvider";
 import { readDocuments } from "../scripts/fireStore/readDocuments";
+import { readLinks } from "../scripts/fireStore/readLinks";
 import Spinner from "../components/shared/Spinner";
 import InvalidID from "../components/shared/InvalidID";
 
@@ -9,7 +10,8 @@ export default function CourseDetails({ collection }) {
   let { id } = useParams();
   const { data, dispatch } = useCourse();
   const [status, setStatus] = useState(0);
-  const currentCourse = data.find((c) => c.id === id);
+  const currentCourse = data.find((course) => course.id === id);
+  const [links, setLinks] = useState([]);
 
   useEffect(() => {
     loadData(collection);
@@ -20,6 +22,8 @@ export default function CourseDetails({ collection }) {
       setStatus(2);
     } else {
       const data = await readDocuments(collection).catch(onFail);
+      var result = await readLinks(`${collection}/${id}/links`);
+      setLinks(result);
       onSuccess(data);
     }
   }
@@ -33,6 +37,14 @@ export default function CourseDetails({ collection }) {
     setStatus(2);
   }
 
+  const selectedLinks = links.map((link) => (
+    <li key={link.id}>
+      <a href={link.link} target="_blank">
+        {link.title}
+      </a>
+    </li>
+  ));
+
   return (
     <div id="CourseDetails">
       {status === 0 && <Spinner />}
@@ -45,8 +57,8 @@ export default function CourseDetails({ collection }) {
           </header>
           <div className="course-content">
             <p>
-              Please check below all materials needed to know how to be
-              professional in this course:
+              Please check below all materials needed to be professional in this
+              course:
             </p>
             <h3>Files</h3>
             <ul>
@@ -58,14 +70,7 @@ export default function CourseDetails({ collection }) {
               </li>
             </ul>
             <h3>Links</h3>
-            <ul>
-              <li>
-                <a>Link 1</a>
-              </li>
-              <li>
-                <a>Link 2</a>
-              </li>
-            </ul>
+            <ul>{selectedLinks}</ul>
           </div>
           <Link to="/courses" className="primary-btn">
             Back

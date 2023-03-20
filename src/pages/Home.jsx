9252487b80ad9d01a94bb2b5e109bdff54f641e6
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-20 01:22:18 +0100

    Add Start Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/home/Hero";
 import SpecialFeatures from "../components/home/SpecialFeatures";
+import Start from "../components/home/Start";
 
 export default function Home() {
   return (
     <div id="Home">
       <Hero />
       <SpecialFeatures />
+      <Start />
     </div>
   );
 }
